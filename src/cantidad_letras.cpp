9541685b918d7ce286b0786f25e98f1ea915f7c0
JoelSlot <42810565+JoelSlot@users.noreply.github.com>
2022-01-05 19:09:21 +0100

    Update cantidad_letras.cpp

--- src/cantidad_letras.cpp
+++ src/cantidad_letras.cpp
@@ -1,11 +1,21 @@
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <vector>
+#include <algorithm>
 #include "diccionario.h"
 #include "bolsaletras.h"
 
 using namespace std;
+
+//Estructura y operaciones para guardar y sortear letras, su cantidad y su puntuacion
+struct letra{
+  char id;
+  int cantidad, puntos;
+};
+
+bool compara_cantidad(letra &a, letra &b) {return a.cantidad<b.cantidad;};
+bool compara_id(letra &a, letra &b) {return a.id<b.id;};
+
 
 int main(int argc, char * argv[]){
 
@@ -18,61 +28,79 @@ int main(int argc, char * argv[]){
   }
 
     ifstream ifDic(argv[1]),
-             ifLet(argv[2]),
-             ifSal(argv[3]);
-    //Todo este segmento es una forma mas resumida de mirar si los archivos se pueden leer.
-    ifstream *ifPunt[3] = {&ifDic, &ifLet, &ifSal};
-    for (int i = 0; i < 3; i++)
-      if(!(*ifPunt[i])){
-        cout << "No puedo abrir el fichero " << argv[i+1] << endl;
-        return 0;
-      }
-    //Creamos un mapa con letras y numero de veces que aparecen las letras
-    map<char,int> caracteres;
-    map<char,int>::iterator it;
-    string line;
-    while (getline(ifLet, line)){
-      caracteres[line[0]] = 0;
+             ifLet(argv[2]);
+    ofstream ofSal(argv[3]);
+
+    if(!ifDic){
+      cout << "No puedo abrir el fichero " << argv[1] << endl;
+      return 0;
     }
-    double totalChar = 0;
+    if(!ifLet){
+      cout << "No puedo abrir el fichero " << argv[2] << endl;
+      return 0;
+    }
+    if(!ofSal){
+      cout << "No puedo abrir el fichero " << argv[3] << endl;
+      return 0;
+    }
+
+
+
+
+    //Creamos un vector con letras y numero de veces que aparecen las letras
+    vector<letra> caracteres;
+    vector<letra>::iterator it;
+    letra aux;
+    string line;
+    bool found;
+    //Leemos valores del archivo de letras, cogiendo solo la primera letra de una linea si hay multiples
+    while (getline(ifLet, line)){
+      aux = {line[0],0,0};
+      caracteres.push_back(aux);
+    }
+
+    //contamos la cantidad de veces que aparece cada letra en el diccionario
     while (getline(ifDic, line)){
       for (int i = 0; i < line.length(); i++){
-        if ((it = caracteres.find(line[i])) != caracteres.end()){
-          caracteres[line[i]] += 1;
-          totalChar++;
+        found = 0;
+        for (int j = 0; j < caracteres.size() || found == 0; j++){
+          if(line[i] == caracteres[j].id){
+            caracteres[j].cantidad += 1;
+            found = 1;
+          }
         }
       }
     }
-    int puntos, frecuenciaMax = 0;
-    for (it = caracteres.begin(); it != caracteres.end(); ++it)
-      if (it->second > frecuenciaMax)
-        frecuenciaMax = it->second;
 
-    cout << frecuenciaMax << endl;
+    //eliminamos los que no aparecen ninguna vez
     for (it = caracteres.begin(); it != caracteres.end(); ++it){
-      //sistema de puntos aun no estoy seguro
-      puntos = 10 - ((it->second / frecuenciaMax) * 10);
-      cout << it->first << " aparece " << it->second << " veces, con " << puntos << " puntos." << endl;
+      if (it->cantidad == 0){
+        caracteres.erase(it);
+      }
+    }
+
+
+    //sorteamos segun cantidad para asignar puntuacion
+    sort(caracteres.begin(), caracteres.end(), compara_cantidad);
+    int cantidad_max = caracteres[caracteres.size()-1].cantidad;
+
+    //asignamos puntuacion
+    int punto;
+    for (double i = 0; i < caracteres.size(); i++){
+      punto = 10 - (caracteres[i].cantidad*10)/(cantidad_max-1);
+      if (punto == 0) punto = 1;
+      caracteres[i].puntos = punto;
+    }
+    //sorteamos alfabeticamente para escribir en el archivo
+    sort (caracteres.begin(), caracteres.end(), compara_id);
+
+    //escribimos en archivo y pantalla
+    ofSal << "#Letra  Cantidad  puntuacion\n";
+    for (int i = 0; i < caracteres.size(); i++){
+      cout << caracteres[i].id << " aparece " << caracteres[i].cantidad << " veces, con " << caracteres[i].puntos << " puntos." << endl;
+      ofSal << caracteres[i].id << "\t" << caracteres[i].cantidad << "\t" << caracteres[i].puntos << "\n";
     }
 
 
     return 0;
 }
-/**
-What this needs to do:
-  read every line
-
-
-#letras
-a
-b
-c
-d
-e
-f
-.
-.
-.
-z
-
-*/
